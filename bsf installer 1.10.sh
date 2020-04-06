8c6f28e05fd00bbeb46f4a67f9d4635e6bfdda51
Bhart sharma <59437165+BhartSharmawhitehathacker@users.noreply.github.com>
2020-04-06 07:51:52 +0530

    Add files via upload

---
+++ bsf installer 1.10.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+red='\033[1;31m'
+blue='\033[1;34m'
+white='\033[1;37m'
+green='\033[1;32m'
+
+
+clear
+printf "$green"
+
+figlet -f big Bsf Installer
+
+
+printf "$green"
+echo " author Bhart Sharma 
+----------------------------------------
+
+
+"
+printf "$red"
+echo " Instagram varun_sharSharma4578
+
+----------------------------------------
+
+"
+
+printf "$blue"
+echo " You Tube Termux Hacker
+---------------------------------------
+
+
+"
+
+echo " Modified By Aniket Stark 
+
+======================================
+
+"
+
+
+echo " loading Menu
+---------------------------------------
+
+
+"
+
+
+echo "=================================
+
+"
+
+echo " 1. Termux All Package Installer
+
+"
+
+echo "2. Follow Me On Instagram 
+
+"
+
+echo "3. Install Metasploit FrameWork
+
+
+"
+
+echo "4. launch Msfconsole
+
+
+"
+
+echo "5. hacker Group
+
+
+"
+
+
+
+echo "6. termux setup Storage
+
+
+
+"
+
+
+echo "7.  Account Hacking Tools
+
+
+"
+
+
+echo " 8. Website Hacking Tools
+
+
+"
+
+echo " 9. bhart Sharma 123 Phone Hacking App
+
+
+"
+
+echo "10.Press Any Key For Exit 
+
+
+"
+
+echo " 11. Device Information
+
+
+
+"
+
+
+
+
+echo "=================================
+
+
+"
+
+echo "//////////////////////////////////////"
+
+read -p ">|" o
+if [ $o = "1" ]
+then
+
+apt update -y
+apt upgrade -y
+apt install python -y 
+apt install python -y 
+apt install curl -y 
+apt install tor -y 
+apt install git -y
+apt install php -y 
+apt install figlet -y 
+apt install toilet -y
+apt install cowsay -y
+apt install perl -y
+apt install nmap -y
+apt install openssh -y
+apt install screenfetch -y
+apt install ruby -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install tar -y
+apt install zip -y
+apt install nano -y
+apt install fish -y
+apt install sudo -y
+apt install wget -y
+apt install w3m -y
+apt install sudo -y
+apt install cmatrix -y
+apt install wcalc -y
+apt install openssl -y
+apt install bmon -y
+
+echo " Now Your Terminal Ready For Use 
+=======================================
+author Bhart Sharma 
+========================================
+follow On Instagram varun_sharma4578
+========================================
+company bsfvenom
+========================================
+
+" 
+
+elif [ $o = "2" ]
+then
+termux-open-url https://www.instagram.com/varun_sharma4578/
+
+elif [ $o = "3" ]
+then
+pkg install metasploit
+pkg install unstable-repo
+
+
+
+elif [ $o = "4" ]
+then
+clear
+msfconsole
+
+elif [ $o = "5" ]
+then
+clear
+
+echo " ( Hacker Group ) 
+
+=======================================
+
+" 
+
+echo "=================================
+
+
+"
+
+
+
+echo " anonymous 
+
+=============================
+
+" 
+
+
+echo " Indian cyber Hacker Team
+
+==================================
+
+
+" 
+
+elif [ $o = "6" ]
+then
+
+termux-setup-storage
+ 
+
+elif [ $o = "7" ]
+then
+git clone https://github.com/thelinuxchoice/shellphish
+
+git clone https://github.com/htr-tech/zphisher
+
+git clone https://github.com/thelinuxchoice/instainsane
+
+
+echo "================================
+
+
+"
+
+
+
+echo " These All Tools Are Not Made By Me 
+
+=======================================
+
+"
+
+elif [ $o = "8" ]
+then
+git clone https://github.com/sqlmapproject/sqlmap
+
+elif [ $o = "9" ]
+then 
+termux-open-url https://bhartsharma123.herokuapp.com/
+
+elif [ $o = "10" ]
+then 
+echo " exiting
+
+"
+exit 
+
+
+elif [ $o = "11" ]
+then
+screenfetch
+
+echo " This is your device Information 
+
+=======================================
+
+"
+fi
